317f35f4ff272fac4d334421bbc4ce64b2dacb50
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-01-28 01:27:25 +0300

    feat: added the active link indicator on the NavBar

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,11 +1,25 @@
+import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
 
-const CustomLink = ({ link, label }) => {
-  return (
-    <Link to={link} smooth>
-      <li className="text-white font-bold">{label}</li>
-    </Link>
-  );
-};
+const CustomLink = React.forwardRef(
+  ({ link, label, onClick, activeLink }, ref) => {
+    // Check if the link is active
+    const isActive = activeLink === link;
+
+    return (
+      <Link
+        to={link}
+        smooth
+        onClick={onClick}
+        className={`${
+          isActive ? "text-background font-bold" : "text-white"
+        } px-[20px]  h-full flex items-center justify-center m-1 rounded-full`}
+        ref={ref}
+      >
+        {label}
+      </Link>
+    );
+  }
+);
 
 export default CustomLink;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,57 @@
+import React, { useRef, useEffect, useState } from "react";
 import CustomLink from "./CustomLink";
 import { links } from "../lib/constants";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Navbar = () => {
+  const [activeLink, setActiveLink] = useState(links[0].href); // default to the first link
+  const linkRefs = useRef(
+    links.reduce(
+      (acc, link) => ({ ...acc, [link.href]: React.createRef() }),
+      {}
+    )
+  );
+
+  useEffect(() => {
+    // force a re-render when the widths change
+    setActiveLink(activeLink);
+  }, [linkRefs.current]);
+
+  const calculatePosition = (link) => {
+    const left = linkRefs.current[link].current.offsetLeft;
+    return left;
+  };
+
+  const calculateWidth = (link) => {
+    return linkRefs.current[link].current.offsetWidth;
+  };
   return (
     <nav className="fixed w-full bg-transparent flex items-center justify-between p-6 border border-green-500">
-      <h1 className="text-xl text-white font-bold text-center">
+      <Link
+        to="#home"
+        smooth
+        className="text-center text-xl font-bold text-white"
+      >
         Christopher <br /> Mungiria
-      </h1>
-      <div className="border border-primary rounded-full">
-        <ul className="flex gap-4 p-2">
-          {links.map((link) => (
-            <CustomLink key={link.href} link={link.href} label={link.name} />
-          ))}
-        </ul>
+      </Link>
+      <div className="flex items-center justify-between border border-primary h-[60px] rounded-full p-[5px] relative">
+        <div
+          className="h-4/5 absolute transition-all duration-300 bg-primary rounded-full z-[-1]"
+          style={{
+            left: calculatePosition(activeLink),
+            width: calculateWidth(activeLink),
+          }}
+        ></div>
+        {links.map((link) => (
+          <CustomLink
+            ref={linkRefs.current[link.href]}
+            key={link.href}
+            link={link.href}
+            label={link.name}
+            onClick={() => setActiveLink(link.href)}
+            activeLink={activeLink}
+          />
+        ))}
       </div>
       <div>download cv</div>
     </nav>
