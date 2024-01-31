0aeeb0475a31d12798a3ff3e67d5c75a08004c3b
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-02-01 02:28:08 +0300

    fix: Images not showing after gh pages hosting

--- src/components/AboutImage.jsx
+++ src/components/AboutImage.jsx
@@ -4,7 +4,7 @@ const AboutImage = () => {
   return (
     <div className="sm:w-[234.31px] sm:h-[200px] w-[175.735px] h-[150px] bg-gradient-to-b from-transparent to-Highlight rounded-[113px]">
       <img
-        src="/assets/about_image.png"
+        src="./assets/about_image.png"
         alt="about_image"
         className="sm:w-full sm:h-full object-cover w-[175.735px] h-[150px]"
       />
--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -4,7 +4,7 @@ const HeroImage = () => {
   return (
     <div className="bg-gradient-to-b from-Highlight h-[320px] w-[192px] md:w-[360px] md:h-[600px] to-transparent rounded-[113px]">
       <img
-        src="/assets/hero_image.png"
+        src="./assets/hero_image.png"
         alt="hero_image"
         className="h-full w-full"
       />
